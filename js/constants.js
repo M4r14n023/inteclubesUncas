203359e8d3ec180bf65e1b99e58af32ab580ac43
Mariano <marianolumbreras@gmail.com>
2026-02-25 17:31:48 -0300

    Interclubes 3.2

--- js/constants.js
+++ js/constants.js
@@ -1,24 +1,27 @@
-export const ID_PROYECTO = 'nombre_del_equipo'; // Asegúrate de que coincida con tu Firebase
+window.AppConstants = {
+    ID_PROYECTO: 'nombre_del_equipo', // Cambia esto por el tuyo
+    
+    CATEGORIAS_DISPONIBLES: [
+        { value: 'caballeros_2da', label: 'Caballeros 2da', max: 5 },
+        { value: 'caballeros_3ra', label: 'Caballeros 3ra', max: 5 },
+        { value: 'caballeros_4ta', label: 'Caballeros 4ta', max: 5 },
+        { value: 'caballeros_5ta', label: 'Caballeros 5ta', max: 5 },
+        { value: 'damas_a',        label: 'Damas A',        max: 3 },
+        { value: 'damas_b',        label: 'Damas B',        max: 4 },
+        { value: 'damas_c',        label: 'Damas C',        max: 4 },
+    ],
 
-export const CATEGORIAS_DISPONIBLES = [
-    { value: 'caballeros_2da', label: 'Caballeros 2da', max: 5 },
-    { value: 'caballeros_3ra', label: 'Caballeros 3ra', max: 5 },
-    { value: 'caballeros_4ta', label: 'Caballeros 4ta', max: 5 },
-    { value: 'caballeros_5ta', label: 'Caballeros 5ta', max: 5 },
-    { value: 'damas_a',        label: 'Damas A',        max: 3 },
-    { value: 'damas_b',        label: 'Damas B',        max: 4 },
-    { value: 'damas_c',        label: 'Damas C',        max: 4 },
-];
+    MAX_POR_CATEGORIA: {
+        'caballeros_2da': 5, 'caballeros_3ra': 5, 'caballeros_4ta': 5, 'caballeros_5ta': 5,
+        'damas_a': 3, 'damas_b': 4, 'damas_c': 4
+    },
 
-export const MAX_POR_CATEGORIA = Object.fromEntries(CATEGORIAS_DISPONIBLES.map(c => [c.value, c.max]));
-export const NOMBRES_CATS = Object.fromEntries(CATEGORIAS_DISPONIBLES.map(c => [c.value, c.label]));
-export const ORDEN_CATS = CATEGORIAS_DISPONIBLES.map(c => c.value);
-export const EQUIPOS_POR_DEFECTO = [ 'Uncas', 'El Rojo', 'Ferro', 'Tercer Tiempo', 'Tres Arroyos', 'Head', 'La Cardoneta', 'La Sede', 'Tiro Federal' ];
-window.AppConstants = { 
-    ID_PROYECTO, 
-    CATEGORIAS_DISPONIBLES, 
-    MAX_POR_CATEGORIA, 
-    NOMBRES_CATS, 
-    ORDEN_CATS, 
-    EQUIPOS_POR_DEFECTO 
+    NOMBRES_CATS: {
+        'caballeros_2da': 'Caballeros 2da', 'caballeros_3ra': 'Caballeros 3ra', 'caballeros_4ta': 'Caballeros 4ta', 'caballeros_5ta': 'Caballeros 5ta',
+        'damas_a': 'Damas A', 'damas_b': 'Damas B', 'damas_c': 'Damas C'
+    },
+
+    ORDEN_CATS: ['damas_a', 'damas_b', 'damas_c', 'caballeros_2da', 'caballeros_3ra', 'caballeros_4ta', 'caballeros_5ta'],
+
+    EQUIPOS_POR_DEFECTO: ['Uncas', 'El Rojo', 'Ferro', 'Tercer Tiempo', 'Tres Arroyos', 'Head', 'La Cardoneta', 'La Sede', 'Tiro Federal']
 };
